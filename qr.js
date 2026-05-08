487eeb55e1678049aa327e261f66cb65c6ecbbb6
TEDDY TECH <kibetke4488@gmail.com>
2026-05-08 17:40:04 +0300

    Update 🫴😂😂

--- qr.js
+++ qr.js
@@ -57,13 +57,13 @@ router.get('/', async (req, res) => {
                                         let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                                         await delay(800);
                                    let b64data = Buffer.from(data).toString('base64');
-                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Teddy-xmd~' + b64data });
+                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Ultra-X:~' + b64data });
         
                                    let MBUVI_MD_TEXT = `
 ╔═══════════════════
 ║『 SESSION CONNECTED』
-║ 🟢  Teddy-Xmd 
-║ 🟢  Teddy
+║ 🟢  June x
+║ 🟢  supreme
 ║ 🟢  base64
 ╚═══════════════════`;
          await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
